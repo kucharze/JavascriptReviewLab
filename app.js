dd4255d3aa71ac7903605e81ee0ee71455643fdf
Zachary Kuchar <zacharyk323@live.com>
2023-06-12 15:47:59 -0400

    finished variable section

--- app.js
+++ app.js
@@ -5,3 +5,20 @@ let item = "Hello";
 
 //2. How do we change the value of a variable
 item = "new";
+
+//3 How do we assign an existing variable to a new variable
+let item2 = item;
+
+//4. What are declare, assign, define
+//Declare - creating a variable
+//Assign - assigning a variable with the = operator
+//Define - assign a variable a value
+
+//5 what is pseudocoding and why should you do it?
+//Giving a coding blueprint to what should happen
+//Gives an idea to build a structure for the code
+
+//6. What percentage of time should be spent thinking about how
+//you're going to solve a problem vs actually typing in code to solve it?
+
+//You should spend about 10 mins solving the problem and then about an hour to write the code
